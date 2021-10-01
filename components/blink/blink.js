5060010f9cc522442771f74a71e424eeb7fd8873
LaJBel Ms <lajbelms@gmail.com>
2021-10-01 15:44:57 -0300

    blink.js

---
+++ components/blink/blink.js
@@ -0,0 +1,15 @@
+function blink(time) {
+    return {
+        id: "blink",
+        require: [],
+        add() {
+            if(this.loopBlink) this.loopBlink();
+        
+            this.loopBlink = loop(time, (time) => {
+                if(!time) time = 0.3;
+
+                this.hidden = !this.hidden;
+            });
+        }
+    };
+};
