4ab21067152325c18c792e38f0055b9d3c09e42c
LaJBel Ms <lajbelms@gmail.com>
2021-09-30 22:57:11 -0300

    openclose file

--- components/openClose.js
+++ components/openClose.js
@@ -1,1 +1,30 @@
+function openClose(config) {
 
+    return {
+
+        id: "openClose",
+        require: [ "sprite" ],
+
+        add() {
+
+            this.openFrequency = config.frequency;
+            this.openChance = config.chance;
+            this.openDuration = config.duration;
+
+            this.open = () => {
+                if(k.rand() > this.openChance) return;
+                this.play("open");
+                k.wait(this.openDuration, this.close);
+            };
+
+            this.close = () => {
+                this.play("close");
+                k.wait(this.openFrequency, this.open);
+            };
+
+            k.wait(this.openFrequency, this.open);
+        },
+
+    };
+
+};
